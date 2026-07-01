f70dc135a81077350087465cae322eebcac233fe
max178b <mabots@neoflex.ru>
2026-07-01 13:35:50 +0400

    +вычисления

--- sandbox/src/main/java/ru/at/test/sandbox/hello.java
+++ sandbox/src/main/java/ru/at/test/sandbox/hello.java
@@ -3,5 +3,10 @@ package ru.at.test.sandbox;
 public class hello {
   public static void main(String[] args) {
     System.out.println("Hello, World!");
+
+    int a=7;
+    int b=10;
+    int s=a*b;
+    System.out.println("Площадь прямоугольника со сторонами " + "a и b" + " = " + s);
   }
 }
