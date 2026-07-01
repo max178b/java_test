564a95f95909c5fb46e9d1972d7e0c51fd494a26
max178b <mabots@neoflex.ru>
2026-07-01 16:37:06 +0400

    добавлено несколько функций

--- sandbox/src/main/java/ru/at/test/sandbox/hello.java
+++ sandbox/src/main/java/ru/at/test/sandbox/hello.java
@@ -2,11 +2,25 @@ package ru.at.test.sandbox;
 
 public class hello {
   public static void main(String[] args) {
-    System.out.println("Hello, World!");
+    hwrld("world");
+    hwrld("user");
+    hwrld("Max");
+    double l = 7;
+    double a = 6;
+    double b = 10;
+    System.out.println("ploshad kvadrata so storonoi "+ l + " = "+ area(l));
+    System.out.println("ploshad pryamougolnika so storonami "+a+" i "+b+" = " + area(a,b));
+  }
 
-    int a=7;
-    int b=10;
-    int s=a*b;
-    System.out.println("Площадь прямоугольника со сторонами " + "a и b" + " = " + s);
+  public static void hwrld(String somebody){
+    System.out.println("Hello, "+ somebody +"!");
+  }
+
+  public static double area(double len){
+    return len*len;
+  }
+
+  public static double area(double a, double b){
+    return a*b;
   }
 }
